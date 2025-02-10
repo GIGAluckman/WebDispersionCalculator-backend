91d8fa78fb1890913038930e40e22b2863b539d7
GIGAluckman <andrey.a.voronov@gmail.com>
2025-02-10 11:30:38 +0100

    Setting gilbert damping

--- TetraxCalc.py
+++ TetraxCalc.py
@@ -44,6 +44,7 @@ class TetraxCalc:
     def set_material(self):
         self.sample.material['Msat'] = float(self.data['saturationMagnetization'])
         self.sample.material['Aex'] = float(self.data['exchangeStiffness'])
+        self.sample.material['alpha'] = float(self.data['GilbertDamping'])
         if 'anisotropyConstant' in self.data.keys():
             self.sample.material['Ku1'] = float(self.data['anisotropyConstant'])
             self.sample.material['e_u'] = self.data['anisotropyAxis']
